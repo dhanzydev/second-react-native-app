e96d47c350c3a427854289a141a25cf858b0ad7e
Dipta Dhananjaya <madedipta88@gmail.com>
2022-09-27 16:02:29 +0800

    materi 3

--- App.js
+++ App.js
@@ -32,6 +32,14 @@ const DATA = [
       judul3: "Manifestasi Klinis",
     },
   },
+  {
+    id: "3",
+    subJudul: {
+      judul1: "Definisi",
+      judul2: "Etiologi",
+      judul3: "Manifestasi Klinis",
+    },
+  },
 ];
 
 const styles = StyleSheet.create({
@@ -358,6 +366,120 @@ function ManifestastiGlomerulonefritis() {
   );
 }
 
+function DefinisiHidronefrosis() {
+  return (
+    <View style={styles.contentWrapper}>
+      <Text style={styles.title}>Definisi</Text>
+      <Text style={styles.materi}>
+        Hidronefrosis adalah dilatasi piala dan perifer ginjal pada satu atau
+        kedua ginjal akibatadanya obstruksi pada aliran normal urin menyebabkan
+        urin mengalir balik sehingga tekanandiginjal meningkat.Hidronefrosis
+        adalah obstruksi aliran kemih proksimal terhadap kandung kemih
+        dapatmengakibatkan penimbunan cairan bertekanan dalam pelviks ginjal dan
+        ureter yang dapatmengakibatkan absorbsi hebat pada parenkim
+        ginjal.Apabila obstruksi ini terjadi di ureter atau kandung kemih,
+        tekanan balik akan mempengaruhikedua ginjal tetapi jika obstruksi
+        terjadi disalah satu ureter akibat adanya batu atau kekakuanmaka hanya
+        satu ginjal yang rusak
+      </Text>
+    </View>
+  );
+}
+
+function EtiologiHidronefrosis() {
+  return (
+    <View style={styles.contentWrapper}>
+      <ScrollView>
+        <Text style={styles.title}>Etiologi</Text>
+        <Text style={styles.materi}>
+          Hidronefrosis biasanya terjadi akibat adanya sumbatan pada sambungan
+          ureteropelvik(sambungan antara ureter dan pelvis renalis):
+        </Text>
+        <Text style={styles.materi}>
+          1) Kelainan struktural, misalnya jika masuknya ureter ke dalam pelvis
+          renalis terlalutinggi
+        </Text>
+        <Text style={styles.materi}>
+          2) Lilitan pada sambungan ureteropelvik akibat ginjal bergeser ke
+          bawah
+        </Text>
+        <Text style={styles.materi}>
+          3) Batu di dalam pelvis renalisd. Penekanan pada ureter oleh:-jaringan
+          fibrosa- arteri atau vena yang letaknya abnormal- tumor.Hidronefrosis
+          juga bisa terjadi akibat adanya penyumbatan dibawah sambungan
+          ureteropelvikatau karena arus balik air kemih dari kandung kemih:a
+          Batu di dalam ureter b. Tumor di dalam atau di dekat ureterc.
+          Penyempitan ureter akibat cacat bawaan, cedera, infeksi, terapi
+          penyinaran atau pembedahan
+        </Text>
+        <Text style={styles.materi}>
+          4) Kelainan pada otot atau saraf di kandung kemih atau uretere.
+          Pembentukan jaringan fibrosa di dalam atau di sekeliling ureter akibat
+          pembedahan,rontgen atau obat-obatan (terutama metisergid)
+        </Text>
+        <Text style={styles.materi}>
+          5) Ureterokel (penonjolan ujung bawah ureter ke dalam kandung kemih)
+        </Text>
+        <Text style={styles.materi}>
+          6) Kanker kandung kemih, leher rahim, rahim, prostat atau organ
+          panggul lain. Sumbatan yang menghalangi aliran air kemih dari kandung
+          kemih ke uretra akibat pembesaran prostat, peradangan atau kankeri.
+          Arus balik air kemih dari kandung kemih akibat cacat bawaan atau
+          cedera
+        </Text>
+        <Text style={styles.materi}>
+          7) Infeksi saluran kemih yang berat, yang untuk sementara waktu
+          menghalangi kontraksiureter.Kadang hidronefrosis terjadi selama
+          kehamilan karena pembesaran rahim menekan ureter.Perubahan hormonal
+          akan memperburuk keadaan ini karena mengurangi kontraksi ureteryang
+          secara normal mengalirkan air kemih ke kandung kemih.Hidronefrosis
+          akan berakhir bila kehamilan berakhir, meskipun sesudahnya pelvis
+          renalis danureter mungkin tetap agak melebar.Pelebaran pelvis renalis
+          yang berlangsung lama dapat menghalangi kontraksi otot ritmis
+          yangsecara normal mengalirkan air kemih ke kandung kemih. Jaringan
+          fibrosa lalu akanmenggantikan kedudukan jaringan otot yang normal di
+          dinding ureter sehingga terjadikerusakan yang menetap
+        </Text>
+      </ScrollView>
+    </View>
+  );
+}
+
+function ManifestastiHidronefrosis() {
+  return (
+    <View style={styles.contentWrapper}>
+      <ScrollView>
+        <Text style={styles.title}>Manifestasi Klinis</Text>
+        <Text style={styles.materi}>
+          Pasien mungkin asimtomatik jika awitan terjadi secara bertahap.
+          Obstruksi akutdapatmenimbulkan rasa sakit dipanggul dan pinggang. Jika
+          terjadi infeksi maja disuria,menggigil,demam dan nyeri tekan serta
+          piuria akan terjadi. Hematuri dan piuriamungkin juga ada. Jikakedua
+          ginjal kena maka tanda dan gejala gagal ginjal kronik akan muncul,
+          seperti:
+        </Text>
+        <Text>{"\n"}</Text>
+        <Text style={styles.materi}>
+          1) Hipertensi (akibat retensi cairan dan natrium).
+        </Text>
+        <Text style={styles.materi}>2) Gagal jantung kongestif.</Text>
+        <Text style={styles.materi}>
+          3) Perikarditis (akibat iritasi oleh toksik uremi).
+        </Text>
+        <Text style={styles.materi}>4) Pruritis (gatal kulit).</Text>
+        <Text style={styles.materi}>
+          5) Butiran uremik (kristal urea pada kulit).
+        </Text>
+        <Text style={styles.materi}>6) Anoreksia, mual, muntah, cegukan.</Text>
+        <Text style={styles.materi}>
+          7) Penurunan konsentrasi, kedutan otot dan kejang.
+        </Text>
+        <Text style={styles.materi}>8) Amenore, atrofi testikuler</Text>
+      </ScrollView>
+    </View>
+  );
+}
+
 function DetailsScreen({ route, navigation }) {
   const { itemId } = route.params;
   if (itemId === DATA[0].id) {
@@ -407,6 +529,31 @@ function DetailsScreen({ route, navigation }) {
           style={styles.subListButton}
         >
           <Text style={styles.buttonText}>{DATA[1].subJudul.judul3}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  } else if (itemId === DATA[2].id) {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Definisi Hidronefrosis")}
+          style={styles.subListButton}
+        >
+          <Text style={styles.buttonText}>{DATA[2].subJudul.judul1}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Etiologi Hidronefrosis")}
+          style={styles.subListButton}
+        >
+          <Text style={styles.buttonText}>{DATA[2].subJudul.judul2}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate("Manifestasi Klinis Hidronefrosis")
+          }
+          style={styles.subListButton}
+        >
+          <Text style={styles.buttonText}>{DATA[2].subJudul.judul3}</Text>
         </TouchableOpacity>
       </View>
     );
@@ -480,6 +627,19 @@ export default function App() {
         <Stack.Screen
           name="Manifestasi Klinis Glomerulonefritis"
           component={ManifestastiGlomerulonefritis}
+        />
+
+        <Stack.Screen
+          name="Definisi Hidronefrosis"
+          component={DefinisiHidronefrosis}
+        />
+        <Stack.Screen
+          name="Etiologi Hidronefrosis"
+          component={EtiologiHidronefrosis}
+        />
+        <Stack.Screen
+          name="Manifestasi Klinis Hidronefrosis"
+          component={ManifestastiHidronefrosis}
         />
       </Stack.Navigator>
     </NavigationContainer>
